cb4bc54d6efd037e28ef4409bd84f705e09c85cb
TornadoWatch <dwightlarkinsjr@gmail.com>
2019-02-03 14:19:05 -0500

    more of the same yes?
    (cherry picked from commit c6b8d33e31d76b40b2ab33ae81e31beae1b83972)

--- common/defines/PFU_Defines.lua
+++ common/defines/PFU_Defines.lua
@@ -117,7 +117,7 @@ NDefines.NMilitary.PLAYER_ORDER_PLANNING_DECAY = 0.01
 NDefines.NMilitary.STRATEGIC_SPEED_BASE = 7.0
 NDefines.NMilitary.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_STRATEGIC_BOMBER = 0.018
 NDefines.NMilitary.UNIT_LEADER_ASSIGN_TRAIT_COST = 0.00 -- Assigning traits is a PITA. Why should it cost anything?
-NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.20			                -- how to weight in highest armor & pen vs the division average
+NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.2			                -- how to weight in highest armor & pen vs the division average
 NDefines.NMilitary.PEN_VS_AVERAGE = 1.0
 
 
