4921efd3dfe3d98f90d339967a1530994bedb1ff
TornadoWatch <dwightlarkinsjr@gmail.com>
2019-05-20 22:58:50 -0400

    naval invasion support superiority toned down to avoid exploits

--- common/defines/GDU_Defines.lua
+++ common/defines/GDU_Defines.lua
@@ -287,7 +287,7 @@ NDefines.NNavy.MISSION_SUPREMACY_RATIOS = { -- supremacy multipliers for differe
 		0.3, -- MINES SWEEPING	
 		0.0, -- TRAIN
 		0.0, -- RESERVE_FLEET
-		1.0, -- NAVAL_INVASION_SUPPORT
+		0.2, -- NAVAL_INVASION_SUPPORT
 }
 	
 NDefines.NNavy.HIGHER_SHIP_RATIO_POSITIONING_PENALTY_FACTOR = 0.45 -- (0.25 -> 0.45) this basically means that if the enemy fleet is 45% the size of your fleet you take maximum positioning penalty from fleet size, about -25% attack, -25% screening, -35% aa. I would avoid increasing the maximum too much since it might to lead to some absurd results
