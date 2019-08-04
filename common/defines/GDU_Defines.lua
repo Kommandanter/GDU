90264ff5e039b4d75c4bd84ce48bdd52fc5a7df6
Beethoven_hoi4 <dermberg@gmail.com>
2019-08-04 16:47:16 +0200

    1) UK gets a mulberry harbours focus which gives large naval invasion bonuses to everyone on the allies.
    2) Germany's decision to annex Vichy is takeable when Italy is more than 1% surrender progress, rather than 25%. The intention is that if i.e. the Allies invade Sicily or Sardinia prior to Torch, that this can be taken. Some people have tried to argue that the Allies should not torch because if they torch, it gives Germany the Vichy industry. This change means it is no longer a valid argument to say that and then to say that instead, the Allies should just invade Italy directly without bothering to Torch. Of course, the Allies can still invade Italy directly if they want, but if they do so it is no longer going to mean that Germany gets less industry than otherwise.
    
    3) The abovementioned decision to take Vichy is moved from Germany to Italy, since it is presumed the Italy player will be paying more attention to Africa etc than the Germany player. Germany might forget to do the decision, Italy is less likely to forget.
    
    4) 1943 Industry techs nerfed slightly more.
    
    5) You can use up to 50 docks per naval production line rather than just 10-15. This is a QOL change in particular for playing countries with a lot of dockyards like USA and Japan. If you have 200 docks, with a limit of 10-15, you need to have up to 20 or so naval production lines. And each time you make a new ship variant, you have to change all of them. So the idea is to reduce the amount of micro that is needed to keep updating all those naval production lines each time you research i.e. a new fire control tech. This does mean that you can complete an individual ship faster, but on average the times of completion of a larger fleet doesn't change, so hopefully this doesn't have too much of an impact other than the QOL aspect for making it easier to micro naval production. This could be reverted if making it quicker to spam out i.e. a carrier or SHBB has strong negative effects, but the QOL benefits are pretty massive in particular for USA/Japan.

--- common/defines/GDU_Defines.lua
+++ common/defines/GDU_Defines.lua
@@ -94,7 +94,9 @@ NDefines.NProduction.ANNEX_FIELD_EQUIPMENT_RATIO = 1.0
 NDefines.NProduction.EQUIPMENT_BASE_LEND_LEASE_WEIGHT = 2.25
 NDefines.NProduction.LICENSE_EQUIPMENT_BASE_SPEED = -0.20
 NDefines.NProduction.LICENSE_EQUIPMENT_TECH_SPEED_PER_YEAR = -0.000
-NDefines.NProduction.CAPITAL_SHIP_MAX_NAV_FACTORIES_PER_LINE = 10 
+NDefines.NProduction.CAPITAL_SHIP_MAX_NAV_FACTORIES_PER_LINE = 50
+NDefines.NProduction.DEFAULT_MAX_NAV_FACTORIES_PER_LINE = 50
+ 
 NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0.0					-- XP cost for adding a new equipment module in an empty slot when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0.0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 0.0				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
