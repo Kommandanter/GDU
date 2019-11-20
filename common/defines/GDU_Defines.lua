e5acd0ea08221f40abbd16dd7a4eb085186a94c3
Beethoven_hoi4 <Beethoven_hoi4>
2019-11-20 17:15:11 -0500

    temp performance stuff in progress

--- common/defines/GDU_Defines.lua
+++ common/defines/GDU_Defines.lua
@@ -19,9 +19,9 @@ NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0				-- Based on number of control
 
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 240 -- these defines are most likely hourly and not daily , rever to 100 and 130 if desyncs during someone lagging for 10 days
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 240
-NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 6
-NDefines.NNavy.NAVAL_COMBAT_RESULT_TIMEOUT_YEARS = 2.00							-- after that many years, we clear the naval combat results, so they don't get stuck forever in the memory.
-NDefines.NNavy.CONVOY_LOSS_HISTORY_TIMEOUT_MONTHS = 3						-- after this many months remove the history of lost convoys to not bloat savegames and memory since there is no way to see them anyway
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 2
+NDefines.NNavy.NAVAL_COMBAT_RESULT_TIMEOUT_YEARS = 0.5							-- after that many years, we clear the naval combat results, so they don't get stuck forever in the memory.
+NDefines.NNavy.CONVOY_LOSS_HISTORY_TIMEOUT_MONTHS = 1						-- after this many months remove the history of lost convoys to not bloat savegames and memory since there is no way to see them anyway
 
 NDefines.NDiplomacy.BASE_STAGE_COUP_DAILY_PP = 999
 NDefines.NDiplomacy.BASE_STAGE_COUP_TOTAL_COST = 0
